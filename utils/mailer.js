1f4cd6ba9e021d8ae811bc12980284c5e8d1bbc1
Ezeh Godwin <EzehGodwin3942@gmail.com>
2025-09-07 21:23:56 +0100

    added resend for email verification

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,22 +1,19 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+// const sendgridTransport = require("nodemailer-sendgrid");
+import { Resend } from "resend";
 
-function getTransporter() {
-  return nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-}
+// function getTransporter() {
+//   return nodemailer.createTransport(
+//     sendgridTransport({
+//       apiKey: process.env.SENDGRID_API_KEY,
+//     })
+//   );
+// }
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendOtpEmail(email, otp) {
-  const transporter = getTransporter();
-  await transporter.verify();
-  return transporter.sendMail({
+ try {
+  const {data,error} = await resend.emails.send({
     from: process.env.EMAIL_USER,
     to: email,
     subject: `NovelHub Account Verification Code`,
@@ -29,8 +26,47 @@ async function sendOtpEmail(email, otp) {
       <br/>
       <p>Thank you for choosing <strong>NovelHub</strong>.</p>
     `,
-    text: `${otp} is your verification code.`,
   });
+   if (error) {
+      console.error('❌ Failed to send email:', error);
+    } else {
+      console.log('✅ Email sent:', data);
+    }
+  } catch (err) {
+    console.error('❌ Error:', err);
+  }
+  
 }
 
-module.exports = { getTransporter, sendOtpEmail };
+module.exports = { sendOtpEmail };
+// const transporter = getTransporter(); // <-- Call it here!
+
+// const info = await transporter.sendMail({
+//   to: email,
+//   from: process.env.EMAIL_USER, // Must be a verified sender in SendGrid!
+//   subject: `NovelHub Account Verification Code`,
+// html: `
+//   <p>Dear User,</p>
+//   <p>Your verification code is:</p>
+//   <h2 style="color:#2c3e50;">${otp}</h2>
+//   <p>Please enter this code within 10 minutes to verify your account.</p>
+//   <p>If you did not request this, please disregard this email.</p>
+//   <br/>
+//   <p>Thank you for choosing <strong>NovelHub</strong>.</p>
+// `,
+// });
+
+// console.log("✅ OTP email sent:", info.messageId);
+//  from: process.env.EMAIL_USER,
+//     to: email,
+//     subject: `NovelHub Account Verification Code`,
+//     html: `
+//       <p>Dear User,</p>
+//       <p>Your verification code is:</p>
+//       <h2 style="color:#2c3e50;">${otp}</h2>
+//       <p>Please enter this code within 10 minutes to verify your account.</p>
+//       <p>If you did not request this, please disregard this email.</p>
+//       <br/>
+//       <p>Thank you for choosing <strong>NovelHub</strong>.</p>
+//     `,
+//     text: `${otp} is your verification code.`,
